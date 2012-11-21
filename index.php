6b2548a10a5af5f44822f064bf552dc2e555b2c2
Nate Hunzaker <nate.hunzaker@gmail.com>
2012-11-21 12:56:32 -0500

    Adds some additional links to home

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
-<a href="robot.html">Robot</a>
-<a href="yeti.html">Yeti</a>
+<a href="robot.html#idle">Robot Idle</a>
+<a href="robot.html#talking">Robot Talking</a>
+<a href="yeti.html#idle">Yeti</a>
+<a href="yeti.html#talking">Yeti</a>
