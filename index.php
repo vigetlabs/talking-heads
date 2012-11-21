686ccb626d8561450e661d5404af1e89984e1c12
Nate Hunzaker <nate.hunzaker@gmail.com>
2012-11-21 12:59:05 -0500

    Fixes link names

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <p><a href="robot.html#idle">Robot Idle</a></p>
 <p><a href="robot.html#talking">Robot Talking</a></p>
-<p><a href="yeti.html#idle">Yeti</a></p>
-<p><a href="yeti.html#talking">Yeti</a></p>
+<p><a href="yeti.html#idle">Yeti Idle</a></p>
+<p><a href="yeti.html#talking">Yeti Talking</a></p>
