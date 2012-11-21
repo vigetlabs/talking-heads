051e532c8f7100d317ad84d9cc9f09828df4264d
Nate Hunzaker <nate.hunzaker@gmail.com>
2012-11-21 16:00:02 -0500

    removes some thumbnails and index.php

--- index.php
+++
@@ -1,4 +0,0 @@
-<p><a href="robot.html#idle">Robot Idle</a></p>
-<p><a href="robot.html#talking">Robot Talking</a></p>
-<p><a href="yeti.html#idle">Yeti Idle</a></p>
-<p><a href="yeti.html#talking">Yeti Talking</a></p>
