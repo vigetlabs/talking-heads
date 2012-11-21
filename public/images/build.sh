83e7d47005cabfbcd25c94d28a4c89d3628d6930
Nate Hunzaker <nate.hunzaker@gmail.com>
2012-11-21 15:55:30 -0500

    updates readme.md

--- public/images/build.sh
+++ public/images/build.sh
@@ -5,13 +5,13 @@ rm -rf robot/compiled
 mkdir robot/compiled
 convert +append robot/idle/*.png robot/compiled/idle.png
 convert +append robot/talking/*.png robot/compiled/talking.png
-convert -append -resize 80% robot/compiled/*.png images/robot.png
+convert -append -resize 80% robot/compiled/*.png robot.png
 
 echo "Creating yeti spritesheet..."
 rm -rf yeti/compiled
 mkdir yeti/compiled
 convert +append yeti/idle/*.png yeti/compiled/idle.png
 convert +append yeti/talking/*.png yeti/compiled/talking.png
-convert -append -resize 30% yeti/compiled/*.png images/yeti.png
+convert -append -resize 30% yeti/compiled/*.png yeti.png
 
 echo "Done!"
--- readme.md
+++ readme.md
@@ -1,5 +1,13 @@
+## Setup Server
+
 ```bash
 npm install -d
 node app
-bash say.sh "Ho ho ho and all that jazz." Fred
+./say.sh "Ho ho ho and all that jazz." Fred
+```
+
+## Recompile spritesheets
+```bash
+cd public/images
+./build.sh
 ```
