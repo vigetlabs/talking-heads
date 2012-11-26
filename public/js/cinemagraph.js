c0f853067c7969363ba924131b6b9a75227307ac
Nate Hunzaker <nate.hunzaker@gmail.com>
2012-11-26 12:11:21 -0500

    Adds some additional clarity and documentation

--- public/js/cinemagraph.js
+++ public/js/cinemagraph.js
@@ -1,30 +1,7 @@
-(function() {
-
-    "use strict";
-
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
-            || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
-
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-                                       timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-}());
+/**
+ * @name Cinemagraph
+ * @desc A keyframe animation library for canvas with background-image fallback
+ */
 
 (function() {
 
@@ -34,8 +11,8 @@
 
         var self = this;
 
-        this.current    = start || canvas.querySelector("[data-animation]").getAttribute("data-animation");
-        this.canvas     = canvas;
+        this.current = start || canvas.querySelector("[data-animation]").getAttribute("data-animation");
+        this.canvas  = canvas;
 
         // Set the background of the canvas to the first frame of the sheet in
         // case canvas is not supported
@@ -46,6 +23,7 @@
         this.canvasSupport = canvas.getContext;
 
         if (this.canvasSupport) {
+            self.fullscreen();
             this.ctx = canvas.getContext('2d');
         }
 
@@ -53,13 +31,22 @@
         this.timestamp  = Date.now();
         this.current_frame = 0;
 
-        this.src = new Image();
-        this.src.src = canvas.getAttribute("data-src");
-
-        this.fullscreen();
-
-        this.src.onload = function() {
+        this.loadSpritesheet(canvas.getAttribute('data-src'), function() {
             self.play(self.current);
+        });
+
+    };
+
+    Cinemagraph.prototype.loadSpritesheet = function(src, callback) {
+
+        var spritesheet = new Image(),
+            self = this;
+
+        spritesheet.src = src;
+
+        spritesheet.onload = function() {
+            self.src = spritesheet;
+            callback();
         };
 
     };
@@ -82,7 +69,7 @@
 
     Cinemagraph.prototype.getAnimations = function() {
 
-        var elements   = this.canvas.querySelectorAll("[data-animation]"),
+        var elements = this.canvas.querySelectorAll("[data-animation]"),
             i = 0,
             len = elements.length,
             animations = {},
@@ -134,7 +121,7 @@
         var x = -(anim.offset.x + (canvas.width * this.current_frame)),
             y = -anim.offset.y;
 
-        if (this.canvas) {
+        if (this.canvasSupport) {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.drawImage(src, x, y);
         } else {
@@ -145,7 +132,11 @@
 
     Cinemagraph.prototype.play = function (keyframe) {
 
-        if (!this.animations[keyframe]) throw 'Animation "' + keyframe + '" does not exist';
+        if (!this.canvasSupport) return;
+
+        if (!this.animations[keyframe]) {
+            throw new Error('Animation "' + keyframe + '" does not exist');
+        }
 
         this.timestamp = Date.now();
         this.current = keyframe;
@@ -153,5 +144,44 @@
         window.requestAnimationFrame(this.update.bind(this));
 
     };
+
+}());
+
+
+/*
+ * Polyfills
+ */
+
+(function() {
+
+    "use strict";
+
+    // Add canvas to older browsers
+    var canvasPolyfill = document.createElement("canvas");
+
+    // Request Animation Frame
+
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+            || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+                                       timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
 
 }());
